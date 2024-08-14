038690305a8c0e1941583e4a2db098a7133fe32f
seongun Kim <seongun44@naver.com>
2024-08-14 14:10:52 +0900

    first commit

---
+++ src/main/java/com/example/Airbicycle/AirbicycleApplication.java
@@ -0,0 +1,13 @@
+package com.example.Airbicycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbicycleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirbicycleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Airbicycle/AirbicycleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Airbicycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbicycleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
